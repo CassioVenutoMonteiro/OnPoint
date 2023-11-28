3dbce577ad4d673697dedd6916f54b75e3b09461
CassioVenutoMonterio <147112257+CassioVenutoMonterio@users.noreply.github.com>
2023-11-28 08:10:02 -0300

    Add files via upload

--- Model/usuario.php
+++ Model/usuario.php
@@ -1,5 +1,23 @@
 <?php
 
+    class Usuario{
 
+        private $idUsuario;
+        private $nome;
+        private $senha;
+        private $email;
+        private $arquivo;
+
+        public function setIdUsuario($idUsuario){$this->idUsuario = $idUsuario;}
+        public function setNome($nome){$this->nome = $nome;}
+        public function setSenha($senha){$this->senha = $senha;}
+        public function setEmail($email){$this->email = $email;}
+        public function setArquivo($arquivo){$this->arquivo = $arquivo;}
+
+        public function getNome(){return $this->nome;}
+        public function getSenha(){return $this->senha;}
+        public function getEmail(){return $this->email;}
+        public function getArquivo(){return $this->arquivo;}
+    }
 
 ?>
